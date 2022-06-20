3798feb393cb10eb6fe3ad6bb7eb2e9321351363
peterrsm <pedrorsmiranda@gmail.com>
2022-06-19 23:50:07 -0300

    + Swagger

--- src/main/java/com/pedro/fork/ForkApplication.java
+++ src/main/java/com/pedro/fork/ForkApplication.java
@@ -2,8 +2,10 @@ package com.pedro.fork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ForkApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pedro/fork/controller/PessoaController.java
+++ src/main/java/com/pedro/fork/controller/PessoaController.java
@@ -2,6 +2,7 @@ package com.pedro.fork.controller;
 
 import com.pedro.fork.model.Pessoa;
 import com.pedro.fork.service.PessoaService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.*;
@@ -27,26 +28,31 @@ public class PessoaController {
     }
 
     @GetMapping("/pessoas")
+    @ApiOperation(value="Listar todas as pessoas")
     public List getPessoas(){
         return service.getPerson(listaPessoas);
     }
 
     @PostMapping("/pessoas")
+    @ApiOperation(value="Cadastrar pessoas")
     public String cadastraPessoa(@RequestBody Pessoa pessoa){
         return service.cadastraPerson(pessoa, listaPessoas);
     }
 
     @GetMapping("/pessoas/{id}")
+    @ApiOperation(value="Apresentar pessoa e seus conhecidos pela posição na lista")
     public Pessoa getPessoa(@PathVariable("id") int id) {
         return service.getPersonById(id, listaPessoas);
     }
 
     @GetMapping("/pessoas/conhecidas/{nome}")
+    @ApiOperation(value="Listar conhecidos")
     public List<String> getConhecidosDeAlguem(@PathVariable("nome") String nome){
         return service.getSomeoneFriends(nome, listaPessoas);
     }
 
     @GetMapping("/pessoas/desconhecidas/{nome}")
+    @ApiOperation(value="Listar desconhecidos que são conectados a quem conhece")
     public List<String> getPessoasRelacionadas(@PathVariable("nome") String nome) {
         try{
             return service.getRelatedPerson(nome, listaPessoas);
